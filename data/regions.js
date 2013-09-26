93c056ef546e1ae4b9d087b018e39cde558e31f6
Edmund von der Burg <evdb@mysociety.org>
2013-09-26 19:43:41 +0100

    Restructure regions, to make it easier to create smaller regions and combine them into bigger ones.

--- data/regions.js
+++ data/regions.js
@@ -1,93 +1,92 @@
 "use strict";
 
-module.exports = {
-  "europe": {
-    countries: [
+var regions = {};
+module.exports = regions;
 
-      // source is http://en.wikipedia.org/wiki/Europe
+regions.europe = {
+  countries: [
+    // source is http://en.wikipedia.org/wiki/Europe
+    'AL', // Albania
+    'AD', // Andorra
+    'AM', // Armenia
+    'AT', // Austria
+    'AZ', // Azerbaijan
+    'BY', // Belarus
+    'BE', // Belgium
+    'BA', // Bosnia and Herzegovina
+    'BG', // Bulgaria
+    'HR', // Croatia
+    'CY', // Cyprus
+    'CZ', // Czech Republic
+    'DK', // Denmark
+    'EE', // Estonia
+    'FI', // Finland
+    'FR', // France
+    'GE', // Georgia (country)
+    'DE', // Germany
+    'GR', // Greece
+    'HU', // Hungary
+    'IS', // Iceland
+    'IE', // Republic of Ireland
+    'IT', // Italy
+    'KZ', // Kazakhstan
+    'LV', // Latvia
+    'LI', // Liechtenstein
+    'LT', // Lithuania
+    'LU', // Luxembourg
+    'MK', // Republic of Macedonia
+    'MT', // Malta
+    'MD', // Moldova
+    'MC', // Monaco
+    'ME', // Montenegro
+    'NL', // Netherlands
+    'NO', // Norway
+    'PL', // Poland
+    'PT', // Portugal
+    'RO', // Romania
+    'RU', // Russia
+    'SM', // San Marino
+    'RS', // Serbia
+    'SK', // Slovakia
+    'SI', // Slovenia
+    'ES', // Spain
+    'SE', // Sweden
+    'CH', // Switzerland
+    'TR', // Turkey
+    'UA', // Ukraine
+    'GB', // United Kingdom
+    'VA', // Vatican City
 
-      'AL', // Albania
-      'AD', // Andorra
-      'AM', // Armenia
-      'AT', // Austria
-      'AZ', // Azerbaijan
-      'BY', // Belarus
-      'BE', // Belgium
-      'BA', // Bosnia and Herzegovina
-      'BG', // Bulgaria
-      'HR', // Croatia
-      'CY', // Cyprus
-      'CZ', // Czech Republic
-      'DK', // Denmark
-      'EE', // Estonia
-      'FI', // Finland
-      'FR', // France
-      'GE', // Georgia (country)
-      'DE', // Germany
-      'GR', // Greece
-      'HU', // Hungary
-      'IS', // Iceland
-      'IE', // Republic of Ireland
-      'IT', // Italy
-      'KZ', // Kazakhstan
-      'LV', // Latvia
-      'LI', // Liechtenstein
-      'LT', // Lithuania
-      'LU', // Luxembourg
-      'MK', // Republic of Macedonia
-      'MT', // Malta
-      'MD', // Moldova
-      'MC', // Monaco
-      'ME', // Montenegro
-      'NL', // Netherlands
-      'NO', // Norway
-      'PL', // Poland
-      'PT', // Portugal
-      'RO', // Romania
-      'RU', // Russia
-      'SM', // San Marino
-      'RS', // Serbia
-      'SK', // Slovakia
-      'SI', // Slovenia
-      'ES', // Spain
-      'SE', // Sweden
-      'CH', // Switzerland
-      'TR', // Turkey
-      'UA', // Ukraine
-      'GB', // United Kingdom
-      'VA', // Vatican City
+    // Several dependencies and similar territories with broad autonomy are also found in Europe. Note that the list does not include the constituent countries of the United Kingdom, federal states of Germany and Austria, and autonomous territories of Spain and the post-Soviet republics as well as the republic of Serbia.
+          // Åland (Finland)
+    'FO', // Faroe Islands (Denmark)
+    'GI', // Gibraltar (UK)
+    'GG', // Guernsey (UK)
+    'IM', // Isle of Man (UK)
+    'JE', // Jersey (UK)
+    'SJ', // Svalbard and Jan Mayen (Norway)
+  ],
+};
 
-      // Several dependencies and similar territories with broad autonomy are also found in Europe. Note that the list does not include the constituent countries of the United Kingdom, federal states of Germany and Austria, and autonomous territories of Spain and the post-Soviet republics as well as the republic of Serbia.
-            // Åland (Finland)
-      'FO', // Faroe Islands (Denmark)
-      'GI', // Gibraltar (UK)
-      'GG', // Guernsey (UK)
-      'IM', // Isle of Man (UK)
-      'JE', // Jersey (UK)
-      'SJ', // Svalbard and Jan Mayen (Norway)
-    ],
-  },
-
-  "middleEast": {
-    countries: [
-      // source is http://en.wikipedia.org/wiki/Middle_east
-      'BH', // Bahrain
-      'CY', // Cyprus
-      'EG', // Egypt
-      'IR', // Iran
-      'IQ', // Iraq
-      'IL', // Israel
-      'JO', // Jordan
-      'KW', // Kuwait
-      'LB', // Lebanon
-      'OM', // Oman
-      'PS', // Palestine
-      'QA', // Qatar
-      'SA', // Saudi Arabia
-      'SY', // Syria
-      'TR', // Turkey
-      'AE', // United Arab Emirates
-      'YE', // Yemen
-    ]
-  }
+regions.middleEast = {
+  countries: [
+    // source is http://en.wikipedia.org/wiki/Middle_east
+    'BH', // Bahrain
+    'CY', // Cyprus
+    'EG', // Egypt
+    'IR', // Iran
+    'IQ', // Iraq
+    'IL', // Israel
+    'JO', // Jordan
+    'KW', // Kuwait
+    'LB', // Lebanon
+    'OM', // Oman
+    'PS', // Palestine
+    'QA', // Qatar
+    'SA', // Saudi Arabia
+    'SY', // Syria
+    'TR', // Turkey
+    'AE', // United Arab Emirates
+    'YE', // Yemen
+  ]
 };
